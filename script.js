4d50c9199a3f1da403ee77b9751123dd966f090f
mustykhot <45202256+mustykhot@users.noreply.github.com>
2020-04-14 15:06:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,45 +3,54 @@ let date = new Date();
 let day = date.getDay();
 let realDay;
 let ginger;
+let whosaidit;
 
 //check day and set qoute
 if(day==0){
     realDay = 'Sunday';
-    ginger = 'Rest small, dont die!';
+    ginger = 'The greatest glory in living lies not in never falling, but in rising every time we fall.';
+    whosaidit = 'Nelson Mandela';
 }
 
 else if(day==1){
-    realDay = 'Na Monday';
-    ginger = 'Ji Masun';
+    realDay = 'Monday';
+    ginger = 'It is during our darkest moments that we must focus to see the light.';
+    whosaidit = 'Aristotle';
 
 }
 
 else if(day==2){
-    realDay = 'Na Tuesday';
-    ginger = 'Work Still Plenty!!';
+    realDay = 'Tuesday';
+    ginger = 'Spread love everywhere you go. Let no one ever come to you without leaving happier.';
+    whosaidit = 'Mother Teresa';
 }
 
 else if(day==3){
-    realDay = 'Na Wednesday';
-    ginger = 'Continue Working';
+    realDay = 'Wednesday';
+    ginger = 'Do not go where the path may lead, go instead where there is no path and leave a trail.';
+    whosaidit = 'Ralph Waldo Emerson';
 }
 
 else if(day==4){
-    realDay = 'Na Thursday';
-    ginger = 'Mafo You will soon rest';
+    realDay = 'Thursday';
+    ginger = 'In the end, it\'s not the years in your life that count. It\'s the life in your years.';
+    whosaidit = 'Abraham Lincoln';
 }
 
 else if(day==5){
-    realDay = 'Na Friday';
-    ginger = 'Oya stop working';
+    realDay = 'Friday';
+    ginger = 'Never let the fear of striking out keep you from playing the game.';
+    whosaidit = 'Babe Ruth';
 
 }
 
 else if(day==6){
-    realDay = 'Na Saturday';
-    ginger = 'Kalo parrryyy!!';
+    realDay = 'Saturday';
+    ginger = 'Life is never fair, and perhaps it is a good thing for most of us that it is not.';
+    whosaidit = 'Oscar Wilde';
 }
 
 //release output
 document.getElementById("d").innerHTML=realDay;
 document.getElementById("quote").innerHTML=ginger;
+document.getElementById('person').innerHTML=whosaidit;
